b0d09e5f15135057a746082fa9286254d4e8d6e7
roarijo89 Tweaks <roarijo89@gmail.com>
2020-10-27 17:48:33 -0400

    dont touch my shit

--- sign.sh
+++ sign.sh
@@ -2,3 +2,7 @@ dpkg-scanpackages -m ./debfiles > Packages
     gzip -c Packages > Packages.gz
     xz -c Packages > Packages.xz
     bzip2 -c Packages > Packages.bz2
+
+git add --all
+git commit -m "dont touch my shit"
+git push
