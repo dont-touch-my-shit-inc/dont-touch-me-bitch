3943854f7c91629c3ea8be1181b2469eedc42cb2
roarijo89 Tweaks <roarijo89@gmail.com>
2020-10-27 17:37:15 -0400

    Dont touch my shit bitch woo

--- sign.sh
+++ sign.sh
@@ -2,7 +2,3 @@ dpkg-scanpackages -m ./debfiles > Packages
     gzip -c Packages > Packages.gz
     xz -c Packages > Packages.xz
     bzip2 -c Packages > Packages.bz2
-
-git add --all
-git commit -m "dont touch my shit"
-git push
